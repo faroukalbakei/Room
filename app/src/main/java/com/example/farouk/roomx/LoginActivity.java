54748112f390473258bb1717f98674e509d7e8fb
farouk <farouk.h@hotmail.com>
2017-02-03 00:14:45 +0200

    tap erorr

--- app/src/main/java/com/example/farouk/roomx/LoginActivity.java
+++ app/src/main/java/com/example/farouk/roomx/LoginActivity.java
@@ -1,10 +1,11 @@
 package com.example.farouk.roomx;
 
+
+
 import android.content.Intent;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -93,18 +94,8 @@ public class LoginActivity extends AppCompatActivity implements VolleyCallback {
         if (isValid==1) {
 
             // Toast.makeText(this, "^_^", Toast.LENGTH_LONG).show();
-           // Intent intent = new Intent(this, Acounting.class);
-          //  startActivity(intent);
-
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            Acounting fragment = new Acounting();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.activity_acounting,fragment);
-            fragmentTransaction.commit();
-           // finish();
-
-
-
+         Intent intent = new Intent(this, IconTextTabsActivity.class);
+          startActivity(intent);
 
 
 
