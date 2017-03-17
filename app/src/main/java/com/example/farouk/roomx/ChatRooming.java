174055414cc127a404b81e0c80034d95c6d476c1
Dev Abir <abirabullah@gmail.com>
2017-03-17 22:48:47 +0200

    first

--- app/src/main/java/com/example/farouk/roomx/ChatRooming.java
+++ app/src/main/java/com/example/farouk/roomx/ChatRooming.java
@@ -9,5 +9,6 @@ public class ChatRooming extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_rooming);
+
     }
 }
